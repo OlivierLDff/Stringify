013f50b723f555067ff4cfb3f0b781ef045c8e08
Olivier LDff <olivier.ldff@gmail.com>
2020-01-13 10:47:10 +0100

    Enum Helper

--- src/include/Stringify/EnumHelper.hpp
+++ src/include/Stringify/EnumHelper.hpp
@@ -1,5 +1,5 @@
-#ifndef __STRINGIFY_ADDRESSREGEX_HPP__
-#define __STRINGIFY_ADDRESSREGEX_HPP__
+#ifndef __STRINGIFY_ENUM_HELPER_HPP__
+#define __STRINGIFY_ENUM_HELPER_HPP__
 
 // ─────────────────────────────────────────────────────────────
 //                  INCLUDE
@@ -27,18 +27,15 @@ STRINGIFY_NAMESPACE_START
 //                  CLASS
 // ─────────────────────────────────────────────────────────────
 
-class AddressRegex
+class EnumHelper
 {
 public:
-    static QString ipv4ToString(const quint32 address);
-    static QString ipv6ToString(const quint8* address);
-    static bool getIpv4(const QString& subject, quint32& address);
-    static bool isIpv6(const QString& subject);
-    static bool getIpv6(const QString& subject, quint8* address);
-    static bool getMacAddress(const QString& subject, quint8* mac);
-    static QString macAddressToString(const quint8* mac);
+    static QString valueToStrWithHex(const QString& s, int value)
+    {
+        return s + QStringLiteral(" (0x") + QString::number(value, 16) + QStringLiteral(")");
+    }
 };
 
 STRINGIFY_NAMESPACE_END
 
-#endif // __STRINGIFY_ADDRESSREGEX_HPP__
+#endif
