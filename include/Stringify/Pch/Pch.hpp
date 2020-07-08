b1956866f47ec5a0249fc5bc14cd0eb92ecb1dc0
Olivier LDff <olivier.ldff@gmail.com>
2020-07-08 13:22:33 +0200

    Ci + PCH

---
+++ include/Stringify/Pch/Pch.hpp
@@ -0,0 +1,34 @@
+#ifndef __STRINGIFY_PCH_HPP__
+#define __STRINGIFY_PCH_HPP__
+
+// ─────────────────────────────────────────────────────────────
+//                  INCLUDE
+// ─────────────────────────────────────────────────────────────
+
+// stl
+#include <string>
+#include <cstdint>
+#include <memory>
+#include <set>
+#include <regex>
+#include <sstream>
+
+// spdlog
+#include <spdlog/logger.h>
+#include <spdlog/sinks/sink.h>
+
+// qt
+#include <QtGlobal>
+#include <QString>
+#include <QQmlEngine>
+#include <QCoreApplication>
+#include <QRegularExpression>
+
+#ifdef major
+#undef major
+#endif
+#ifdef minor
+#undef minor
+#endif
+
+#endif
