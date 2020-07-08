3ece097f274f6986a39d8cd35787a626028366e9
Olivier LDff <olivier.ldff@gmail.com>
2020-07-08 13:27:11 +0200

    pch

--- include/Stringify/Pch/Pch.hpp
+++ include/Stringify/Pch/Pch.hpp
@@ -5,52 +5,24 @@
 //                  INCLUDE
 // ─────────────────────────────────────────────────────────────
 
-// Types
-#include <cstdint>
-#include <cstddef>
+// stl
 #include <string>
-
-// Containers
+#include <cstdint>
+#include <memory>
 #include <set>
-#include <unordered_set>
-#include <map>
-#include <unordered_map>
-#include <vector>
-#include <list>
-#include <stack>
-#include <queue>
-
-// Utilities
-#include <chrono>
-#include <initializer_list>
-#include <utility>
+#include <regex>
+#include <sstream>
 
 // spdlog
 #include <spdlog/logger.h>
 #include <spdlog/sinks/sink.h>
 
-// flatbuffers
-#include <flatbuffers/flatbuffers.h>
-
-// eventpp
-#include <eventpp/callbacklist.h>
-
-// Unique
-#include <Unique/Unique.hpp>
-
-// Qt Headers
-#include <QAbstractListModel>
-#include <QObject>
-#include <QByteArray>
-#include <QHash>
-#include <QList>
-#include <QMetaObject>
-#include <QMetaProperty>
+// qt
+#include <QtGlobal>
 #include <QString>
-#include <QVariant>
-#include <QDebug>
 #include <QQmlEngine>
-#include <QJSValue>
+#include <QCoreApplication>
+#include <QRegularExpression>
 
 #ifdef major
 #undef major
